35ea63306e2aa050baf04d48e3c79e1b872887d4
geunheeJung <geuni1013@gmail.com>
2018-03-21 04:52:58 +0000

    wp-config 수정

--- wp-config.php
+++ wp-config.php
@@ -1,23 +1,15 @@
-<?php
+<?php 
 /**
- * The base configuration for WordPress
+ * The base configurations of the WordPress.
  *
- * The wp-config.php creation script uses this file during the
- * installation. You don't have to use the web site, you can
- * copy this file to "wp-config.php" and fill in the values.
- *
- * This file contains the following configurations:
- *
- * * MySQL settings
- * * Secret keys
- * * Database table prefix
- * * ABSPATH
- *
- * @link https://codex.wordpress.org/Editing_wp-config.php
+ * This file is a custom version of the wp-config file for multiple
+ * environment.
+ * Inspired by https://github.com/Abban/WordPress-Config-Bootstrap
  *
  * @package WordPress
  */
-
+// determine PHP is running as standalone or apache module
+// if not set just assume PHP run with build-int web server.
 if (!function_exists(apache_getenv)){
     define('WP_ENV', 'local');
 }
@@ -29,42 +21,31 @@ else {
         define('WP_ENV', apache_getenv ('WP_ENV'));
     }
 }
-
-// ** MySQL settings - You can get this info from your web host ** //
-
-
-
-/** MySQL hostname */
+/**#@-*/
 /**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each
- * a unique prefix. Only numbers, letters, and underscores please!
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
  */
 $table_prefix  = 'wp_';
-
+/**
+ * WordPress Localized Language, defaults to English.
+ *
+ * Change this to localize WordPress. A corresponding MO file for the chosen
+ * language must be installed to wp-content/languages. For example, install
+ * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
+ * language support.
+ */
+define('WPLANG', '');
 /**
  * For developers: WordPress debugging mode.
  *
  * Change this to true to enable the display of notices during development.
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the Codex.
- *
- * @link https://codex.wordpress.org/Debugging_in_WordPress
-
+ */
+define('WP_DEBUG', false);
 /* That's all, stop editing! Happy blogging. */
-/**
- * The WP_SITEURL and WP_HOME options are configured to access from any hostname or IP address.
- * If you want to access only from an specific domain, you can modify them. For example:
- *  define('WP_HOME','http://example.com');
- *  define('WP_SITEURL','http://example.com');
- *
-*/
-
-define('WPLANG', '');
+/** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
     define('ABSPATH', dirname(__FILE__) . '/');
 // load config for multiple environment
@@ -73,41 +54,3 @@ file_exists($config_file) || die ("'$config_file' Not Found!");
 require_once($config_file);
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
-
-if ( defined( 'WP_CLI' ) ) {
-    $_SERVER['HTTP_HOST'] = 'localhost';
-}
-
-define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/');
-define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] . '/');
-
-
-/** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
-
-/** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
-
-define('WP_TEMP_DIR', '/opt/bitnami/apps/wordpress/tmp');
-
-
-define('FS_METHOD', 'direct');
-
-
-//  Disable pingback.ping xmlrpc method to prevent Wordpress from participating in DDoS attacks
-//  More info at: https://docs.bitnami.com/?page=apps&name=wordpress&section=how-to-re-enable-the-xml-rpc-pingback-feature
-
-if ( !defined( 'WP_CLI' ) ) {
-    // remove x-pingback HTTP header
-    add_filter('wp_headers', function($headers) {
-        unset($headers['X-Pingback']);
-        return $headers;
-    });
-    // disable pingbacks
-    add_filter( 'xmlrpc_methods', function( $methods ) {
-            unset( $methods['pingback.ping'] );
-            return $methods;
-    });
-    add_filter( 'auto_update_translation', '__return_false' );
-}
